d0190804e16418982000396c0683e8d8a8e05be9
Labadmin <CooperDThomas2020@gmail.com>
2024-11-13 11:01:33 -0500

    MongoDB Working?

--- app/src/main/java/com/example/xspace/LoginDB.java
+++ app/src/main/java/com/example/xspace/LoginDB.java
@@ -1,70 +1,51 @@
 package com.example.xspace;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.content.ContentValues;
-import android.database.Cursor;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import com.mongodb.client.model.Filters;
 
-public class LoginDB extends SQLiteOpenHelper {
+public class LoginDB {
 
-    private static final String DATABASE_NAME = "UserDatabase.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "UserDatabase";
+    private static final String COLLECTION_NAME = "UserCollection";
 
-    private static final String TABLE_NAME = "UserCollection";
-    private static final String COLUMN_USERNAME = "USERNAME";
-    private static final String COLUMN_PASSWORD = "PASSWORD";
-    private static final String COLUMN_EMAIL = "EMAIL";
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> collection;
 
-    public LoginDB(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (" +
-                COLUMN_USERNAME + " TEXT PRIMARY KEY, " +
-                COLUMN_PASSWORD + " TEXT, " +
-                COLUMN_EMAIL + " TEXT)";
-        db.execSQL(createTable);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
+    public LoginDB(Login login) {
+        mongoClient = MongoClients.create("mongodb+srv://<username>:<password>@cluster0.mongodb.net/?retryWrites=true&w=majority");
+        database = mongoClient.getDatabase(DATABASE_NAME);
+        collection = database.getCollection(COLLECTION_NAME);
     }
 
     public boolean insertData(String username, String password, String email) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(COLUMN_USERNAME, username);
-        contentValues.put(COLUMN_PASSWORD, password);
-        contentValues.put(COLUMN_EMAIL, email);
-        long result = db.insert(TABLE_NAME, null, contentValues);
-        return result != -1; // insertion is successful if result is not -1
+        Document document = new Document("USERNAME", username)
+                .append("PASSWORD", password)
+                .append("EMAIL", email);
+        collection.insertOne(document);
+        return true; // insertion is successful if no exception is thrown
     }
 
     public boolean checkEmailExists(String email) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_NAME, new String[]{COLUMN_EMAIL}, COLUMN_EMAIL + "=?",
-                new String[]{email}, null, null, null);
-        boolean exists = cursor.getCount() > 0;
-        cursor.close();
-        return exists;
+        Bson filter = Filters.eq("EMAIL", email);
+        long count = collection.countDocuments(filter);
+        return count > 0;
     }
 
     public boolean validateUser(String username, String password) {
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_NAME, null, COLUMN_USERNAME + "=? AND " + COLUMN_PASSWORD + "=?",
-                new String[]{username, password}, null, null, null);
-        boolean valid = cursor.getCount() > 0;
-        cursor.close();
-        return valid;
+        Bson filter = Filters.and(Filters.eq("USERNAME", username), Filters.eq("PASSWORD", password));
+        long count = collection.countDocuments(filter);
+        return count > 0;
     }
 
     public void close() {
-        this.close();
+        mongoClient.close();
     }
 }
+
 
---
+++ app/src/main/java/com/example/xspace/mapTracker.java
@@ -0,0 +1,21 @@
+package com.example.xspace;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mapTracker extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maptracker);
+
+
+
+
+    }
+}
